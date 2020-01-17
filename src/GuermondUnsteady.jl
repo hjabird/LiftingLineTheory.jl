b883c086ed81b7cef8e70a6301f2b1d43938fccd
HJA Bird <9040797+hjabird@users.noreply.github.com>
2020-01-17 15:57:55 +0000

    Add Guermond code - actually invalid

--- src/GuermondUnsteady.jl
+++ src/GuermondUnsteady.jl
@@ -16,6 +16,31 @@ mutable struct GuermondUnsteady
     pitch_plunge :: Int64           # Plunge = 3, Pitch = 5. Otherwise invalid.
 
     chord_len_scale :: Float64
+    span_len_scale :: Float64
+    guermond_k :: Float64
+    guermond_nu :: Float64
+
+    function GuermondUnsteady(
+        angular_fq::Real, wing::StraightAnalyticWing;
+        free_stream_vel ::Real = 1, amplitude_fn::Function=x->1,
+        pitch_plunge::Int=3, chord_len_scale::Real=NaN,
+        span_len_scale::Real=NaN, guermond_k::Real=NaN,
+        guermond_nu::Real=NaN)
+
+        @assert(wing.chord_fn(0) == wing.chord_fn(1e-6), "Rect. only")
+        chord_len_scale = (chord_len_scale == chord_len_scale ?
+            chord_len_scale : wing.chord_fn(0))
+        span_len_scale = (span_len_scale == span_len_scale ?
+            span_len_scale : wing.semispan)
+        guermond_k = (guermond_k == guermond_k ?
+            guermond_k : angular_fq * chord_len_scale / free_stream_vel)
+        guermond_nu = (guermond_nu == guermond_nu ?
+            guermond_nu : angular_fq * span_len_scale / free_stream_vel)
+
+        return new(Float64(angular_fq), Float64(free_stream_vel), wing, 
+            amplitude_fn, pitch_plunge, chord_len_scale, span_len_scale,
+            guermond_k, guermond_nu)
+    end
 end
 
 function Y_to_y(a::GuermondUnsteady, Y::Real)
@@ -34,30 +59,18 @@ function y_to_Y(a::GuermondUnsteady, y::Vector{<:Real})
     return y .* a.wing.semispan
 end
 
-function compute_chord_len_scale!(a::GuermondUnsteady)
-    a = area(a.wing)
-    s = a.wing.semispan
-    c = a/(2*s)
-    a.chord_len_scale = c
-    return
-end
-
-function X_to_x(a::GuermondUnsteady, X::Real)
-    return X / a.chord_len_scale
-end
-
-function X_to_x(a::GuermondUnsteady, X::Vector{<:Real})
-    return X ./ a.chord_len_scale
-end
-
 function upwash_distribution(a::GuermondUnsteady, Y::Real; order::Int=1)
+    #= The upwash distribution is a combination of the 2D upwash and, if
+    order = 1, the 3D correction. Input are given with respect to unscaled
+    coordinates and the output is also with respect to unscaled coordinates =#
+
     @assert(abs(Y) < a.wing.semispan)
     @assert(order >= 0, "Positive order required")
     @assert(order < 2, "Only programmed to order 1!")
     bv = NaN
     U = a.free_stream_vel
-    chord = a.wing.chord_fn(Y)
-    k = a.angular_fq * chord / (2* a.free_stream_vel)
+    chord = a.chord_len_scale # Should equal chord for rect wing.
+    k = a.angular_fq * chord / (2* a.free_stream_vel) # Not the same as guermond_k
     amp = a.amplitude_fn(Y)
     # 0th order: 2D
     if a.pitch_plunge == 3
@@ -67,13 +80,15 @@ function upwash_distribution(a::GuermondUnsteady, Y::Real; order::Int=1)
         uw = make_pitch_function(HarmonicUpwash2D, amp, k;
             free_stream_vel = a.free_stream_vel, semichord=chord/2)
     end
-    # 1st order: 3D 
-    if order > 0
+    # 1st order: 3D - the more complicated bit. 
+    if order > 0 # or == 1
         ar = aspect_ratio(a.wing)
         pd_coeff = operator_K1_excl_exp_term(a, Y)
-        uws = make_sinusoidal_gust_function(HarmonicUpwash2D, # THIS BIT I SHOULD BE CAREFUL ABOUT...
-            -pd_coeff, k, U, semichord=chord / a.chord_len_scale)
-        uw = uw - uws / ar
+        uws = make_sinusoidal_gust_function(HarmonicUpwash2D,
+            pd_coeff, k; free_stream_vel= U, semichord=chord/2)
+        #uws = make_plunge_function(HarmonicUpwash2D, pd_coeff / a.angular_fq, k;
+        #    free_stream_vel = a.free_stream_vel, semichord=chord/2)
+        uw = uw - uws/ ar
     end
     return uw
 end
@@ -81,27 +96,41 @@ end
 # Currently assumes rectangular wing!
 function operator_K1_excl_exp_term(a::GuermondUnsteady, Y::Real)
     y = Y_to_y(a, Y)
-    nu = a.angular_fq * a.wing.semispan / a.free_stream_vel
-    k =  a.angular_fq * a.chord_len_scale / a.free_stream_vel
-    c = a.wing.chord_fn(y)
-    G = bound_vorticity(a, y; order=0) * exp(im * k * c / 2)
+    @assert(abs(y) < 1)
+    nu = a.guermond_nu
+    k =  a.guermond_k
+    C = a.chord_len_scale
+    # The bound vorticity in G&S is calculated according to the normalised
+    # problem, but here we are using Kussner-Schwarz to do the work so 
+    # we must divide through by U. I think.
+    G = bound_vorticity(a, y; order=0) * exp(im * k * C / 2) / a.free_stream_vel
 
     # The nasty to compute constant bit
-    t1 = 1 / (2*pi*(y^2 - 1))
-    # See https://www.cs.uaf.edu/~bueler/M611heaviside.pdf
-    t2 = -im * nu / (2*pi*(y^2 - 1)) * 1 / (im * nu)
+    t1 = 1 / (2 * pi)
+    t21 = 1 / (y^2 - 1)
     
-    # t3 requires numerical integration
+    t221 = - im * nu / 2
+    # t22 requires numerical integration
     function integrand(v::Real)
-        t1 = exp(im * nu * v) / v
-        t21 = sqrt(v^2 + (y-1)^2) / (y-1)
-        t22 = -sqrt(v^2 + (y+1)^2) / (y+1)
-        t23 = 2 
-        t24 = 2 * v / (y^2 - 1)
-        return t1 * (t21 + t22 + t23 + t24)
+        #tk1 = 1 / v
+        #tk21 = sqrt(v^2 + (y-1)^2) / (y-1)
+        #tk22 = -sqrt(v^2 + (y+1)^2) / (y+1)
+        #tk23 = 2
+        #tk24 = 2 * v / (y^2 - 1)
+        #tk0 = tk1 * (tk21 + tk22 + tk23 + tk24)
+        #Float64(tk0)
+        #return tk0
+        # *exp(im * nu * v) oscl part in quadrature
+        ti21 = sqrt(1/v^2 + 1/(y-1)^2)
+        ti22 = sqrt(1/v^2 + 1/(y+1)^2)
+        ti23 = 2 / v
+        ti3 = 2 / (y^2 - 1)
+        return  ti21 + ti22 + ti23 + ti3
     end
-    t3 = -im * nu / (4 * pi) * QuadGK.quadgk(integrand, -Inf, 0)[1]
-    constpart = t1 + t2 + t3
+    t222 = fejer_quadrature_exp(integrand, nu, -Inf, -1e-8; finite=true)
+    @assert(isfinite(t222), "Term 3 (the integral) isn't finite!")
+    constpart = t1 * (t21 + t221 * t222)
+    #println("At k = ", k, ": t1 = ", t1*t21,", t2 = ", abs(t1 * t221 * t222))
 
     # The sinusoidal inner solution downwash's coefficient
     coeff = G * constpart
@@ -126,9 +155,10 @@ end
 
 function lift_coefficient(a::GuermondUnsteady; order=1)
     s = a.wing.semispan
-    points, weights = FastGaussQuadrature.gausslegendre(40)
+    points, weights = FastGaussQuadrature.gausslegendre(100)
     points, weights = linear_remap(points, weights, -1, 1, -s, s)
-    CL = sum(weights .* map(Y->lift_coefficient(a, Y; order=order), points))
+    lctc = Y->lift_coefficient(a, Y; order=order) * a.wing.chord_fn(Y)
+    CL = sum(weights .* map(lctc, points)) / area(a.wing)
     return CL
 end
 
