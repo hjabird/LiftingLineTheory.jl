b34908e5801fc9f61aa796fe8ec07973d0ccd74b
HJA Bird <9040797+hjabird@users.noreply.github.com>
2020-02-26 15:23:58 +0000

    Update to new object names

--- scripts/SclavounosFig7and8.jl
+++ scripts/SclavounosFig7and8.jl
@@ -39,33 +39,34 @@ let
         stcl_abs[i] = abs(stcl)
         stcl_ph[i] = atan(imag(stcl), real(stcl))
 
-        prob = HarmonicULLT(omegas[i], wing; downwash_model=streamwise_filaments)
+        prob = HarmonicULLT2(omegas[i], wing; downwash_model=streamwise_filaments)
         compute_collocation_points!(prob)
         compute_fourier_terms!(prob)
         swfcl = lift_coefficient(prob)
         swfcl_abs[i] = abs(swfcl)
         swfcl_ph[i] = atan(imag(swfcl), real(swfcl))
-
-        prob = HarmonicULLT3(omegas[i], wing; num_terms = 9)
+#=
+        prob = SclavounosULLT(omegas[i], wing; num_terms = 9)
         compute_collocation_points!(prob)
         compute_fourier_terms!(prob)
         cl3 = lift_coefficient(prob)
         cl_abs3[i] = abs(cl3)
         cl_ph3[i] = atan(imag(cl3), real(cl3))
 
-        prob = HarmonicULLT3(omegas[i], wing; downwash_model=strip_theory)
+        prob = ScalounosULLT(omegas[i], wing; downwash_model=strip_theory)
         compute_collocation_points!(prob)
         compute_fourier_terms!(prob)
         stcl3 = lift_coefficient(prob)
         stcl_abs3[i] = abs(stcl3)
         stcl_ph3[i] = atan(imag(stcl3), real(stcl3))
 
-        prob = HarmonicULLT3(omegas[i], wing; downwash_model=streamwise_filaments)
+        prob = ScalounosULLT(omegas[i], wing; downwash_model=streamwise_filaments)
         compute_collocation_points!(prob)
         compute_fourier_terms!(prob)
         swfcl3 = lift_coefficient(prob)
         swfcl_abs3[i] = abs(swfcl3)
         swfcl_ph3[i] = atan(imag(swfcl3), real(swfcl3))
+        =#
     end
 
     figure()
@@ -75,9 +76,9 @@ let
     plot(srfs, cl_abs, label="ULLT")
     plot(srfs, stcl_abs, label="Strip theory")
     plot(srfs, swfcl_abs, label="Streamwise filemants")
-    plot(srfs, cl_abs3, label="ULLT3")
-    plot(srfs, stcl_abs3, label="Strip theory3")
-    plot(srfs, swfcl_abs3, label="Streamwise filemants3")
+    #plot(srfs, cl_abs3, label="ULLT3")
+    #plot(srfs, stcl_abs3, label="Strip theory3")
+    #plot(srfs, swfcl_abs3, label="Streamwise filemants3")
     xlabel("Span reduced frequency")
     ylabel("ABS(C_L)")
 
@@ -88,9 +89,9 @@ let
     plot(srfs, ((rad2deg.(cl_ph) .+ 360) .% 360) .-360, label="ULLT")
     plot(srfs, ((rad2deg.(stcl_ph) .+ 360) .% 360) .-360, label="Strip theory")
     plot(srfs, ((rad2deg.(swfcl_ph) .+ 360) .% 360) .-360, label="Streamwise filemants")
-    plot(srfs, ((rad2deg.(cl_ph3) .+ 360) .% 360) .-360, label="ULLT3")
-    plot(srfs, ((rad2deg.(stcl_ph3) .+ 360) .% 360) .-360, label="Strip theory3")
-    plot(srfs, ((rad2deg.(swfcl_ph3) .+ 360) .% 360) .-360, label="Streamwise filemants3")
+    #plot(srfs, ((rad2deg.(cl_ph3) .+ 360) .% 360) .-360, label="ULLT3")
+    #plot(srfs, ((rad2deg.(stcl_ph3) .+ 360) .% 360) .-360, label="Strip theory3")
+    #plot(srfs, ((rad2deg.(swfcl_ph3) .+ 360) .% 360) .-360, label="Streamwise filemants3")
     xlabel("Span reduced frequency")
     ylabel("Ph(C_L)")
     return srfs, cl_abs, cl_ph
